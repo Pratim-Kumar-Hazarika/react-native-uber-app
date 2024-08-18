e30b66bdf01ced2920b3633a7162799d8e758e0f
Pratim <hpro373@gmail.com>
2024-08-19 00:00:41 +0530

    Update welcome.tsx

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,13 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center justify-between">
       <TouchableOpacity
@@ -12,6 +16,23 @@ const Onboarding = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        loop={false}
+        onIndexChanged={(index) => setCurrentSlide(index)}
+      >
+        {onboarding.map((item) => (
+          <View className="">
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
