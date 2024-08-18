7bfadd9281a5b0b16ea6027e16bdf6e964e7bd36
Pratim <hpro373@gmail.com>
2024-08-19 00:26:40 +0530

    add image + button component

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,13 +1,15 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React, { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
 import { onboarding } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const isLastSlide = currentSlide === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between">
       <TouchableOpacity
@@ -28,11 +30,32 @@ const Onboarding = () => {
         onIndexChanged={(index) => setCurrentSlide(index)}
       >
         {onboarding.map((item) => (
-          <View className="">
-            <Text>{item.title}</Text>
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black  text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="  text-[#858585] text-lg  font-JakartaSemiBold mx-10 mt-3 text-center">
+              {item.description}
+            </Text>
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        className="w-11/12 mt-10"
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+      />
     </SafeAreaView>
   );
 };
