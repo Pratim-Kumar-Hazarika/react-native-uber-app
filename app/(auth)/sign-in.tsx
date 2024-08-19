817f587407b1f8af7945e8da26786863355f8040
Pratim <hpro373@gmail.com>
2024-08-19 13:44:40 +0530

    Update sign-in.tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,80 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  async function onSignInPress() {}
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        automaticallyAdjustContentInsets
+        automaticallyAdjustKeyboardInsets={true}
+        className="flex-1  bg-white"
+      >
+        <View className="flex-1 bg-white">
+          <View className="relative w-full h-[250px]">
+            <Image
+              source={images.signUpCar}
+              className="z-0 w-full h-[250px] "
+            />
+            <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+              Welcome 👋
+            </Text>
+          </View>
+          <View className="p-5 flex-1 justify-between">
+            <InputField
+              label="Email"
+              placeholder="Enter email"
+              icon={icons.email}
+              textContentType="emailAddress"
+              value={form.email}
+              onChangeText={(value) => setForm({ ...form, email: value })}
+            />
+            <InputField
+              label="Password"
+              placeholder="Enter password"
+              icon={icons.lock}
+              secureTextEntry={true}
+              textContentType="password"
+              value={form.password}
+              onChangeText={(value) => setForm({ ...form, password: value })}
+            />
+            <CustomButton
+              title="Sign In"
+              onPress={onSignInPress}
+              className="mt-6"
+            />
+            <OAuth />
+            <Link
+              href="/sign-up"
+              className="text-lg  text-center text-general-200 mt-10"
+            >
+              Don't have an account?{" "}
+              <Text className="text-primary-500">Sign Up</Text>
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
