fb7a3aad311a966141f25a4fc7cca08d49b5ad3c
Pratim <hpro373@gmail.com>
2024-08-19 17:18:29 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
-import { Text } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Redirect } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
