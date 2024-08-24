3bd748fd2263aac051065b16575972959a582b1b
Pratim <hpro373@gmail.com>
2024-08-24 15:17:04 +0530

    Update OAuth.tsx

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,9 +1,26 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Alert } from "react-native";
 import React from "react";
 import CustomButton from "./CustomButton";
 import { icons } from "@/constants";
+import { useOAuth } from "@clerk/clerk-expo";
+import { router } from "expo-router";
+import { googleOAuth } from "@/lib/auth";
 
 const OAuth = () => {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleSignIn = React.useCallback(async () => {
+    try {
+      const result = await googleOAuth(startOAuthFlow);
+      if (result.code === "session_exists") {
+        Alert.alert("Success", "Session exists. Redirecting to home page.");
+        router.push("/(root)/(tabs)/home");
+      }
+      Alert.alert(result?.success ? "Success" : "Error", result?.message);
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View>
       <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
@@ -22,6 +39,7 @@ const OAuth = () => {
             className="w-5 h-5 mx-2"
           />
         )}
+        onPress={handleGoogleSignIn}
         textVariant="primary"
       />
     </View>
