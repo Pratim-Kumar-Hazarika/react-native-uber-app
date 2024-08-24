a5246d4e84e754edeecd75bfb242aab272263e8e
Pratim <hpro373@gmail.com>
2024-08-24 18:06:12 +0530

    Update home.tsx

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -101,7 +101,7 @@ const Home = () => {
           <>
             <View className="flex flex-row items-center justify-between my-5">
               <Text className="text-2xl font-JakartaExtraBold">
-                Welcome {user?.firstName}👋
+                Welcome, {user?.firstName}
               </Text>
               <TouchableOpacity
                 onPress={handleSignOut}
